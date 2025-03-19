fabd9d63548e883a8d6297f1b9efd2ac891809a1
ax0788 <ahmy78@gmail.com>
2025-03-19 11:51:41 +0300

    login and registration form css design w/ bootstrap

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -28,7 +28,7 @@ class RegistrationFormType extends AbstractType
             ])
             ->add('firstName')
             ->add('lastName')
-            ->add('plainPassword', PasswordType::class, [
+            ->add('Password', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
